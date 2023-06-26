234db1561d14d3776c684b89be1bc4951c272a09
lhrbu <lihaoran228@163.com>
2023-06-26 09:59:12 +0800

    add chunksize in read_csv to reduce memory use

--- async_program.py
+++ async_program.py
@@ -17,23 +17,23 @@ connection_builder = MockInfluxDBConnectionBuilder()
 async def main():
     async with (InfluxDBClientAsyncWriter(connection_builder,"python_test8")) as writer:
         influxdb_row_parser:IInfluxDBRowParser = BPlusInfluxDBRowParser()
-
         buffered_tasks = []
         with ExecutionTimer() as execution_timer:
-            data = pandas.read_csv("data.csv",sep=",",header="infer")
-            for index,row in data.iterrows():
-                try:
-                    point = influxdb_row_parser.Parse("TAVASCAN L02_09_03_2023_10h39m40ff_EXCEL-Daten",row,data.columns)
-                    task = writer.WritePoint(point)
-                    buffered_tasks.append(task)
-                    if len(buffered_tasks)>=MAX_CONCURRENT_COUNT :
-                        await asyncio.wait(buffered_tasks)
-                        buffered_tasks.clear()
-                except Exception as exception:
-                    print(exception)
-                    continue
-            if(len(buffered_tasks)>0):
-                await asyncio.wait(buffered_tasks)
-                buffered_tasks.clear()
+            chunks = pandas.read_csv("data.csv",sep=",",header="infer",chunksize=10000)
+            for chunk in chunks:
+                for index,row in chunk.iterrows():
+                    try:
+                        point = influxdb_row_parser.Parse("TAVASCAN L02_09_03_2023_10h39m40ff_EXCEL-Daten",row,chunk.columns)
+                        task = writer.WritePoint(point)
+                        buffered_tasks.append(task)
+                        if len(buffered_tasks)>=MAX_CONCURRENT_COUNT :
+                            await asyncio.wait(buffered_tasks)
+                            buffered_tasks.clear()
+                    except Exception as exception:
+                        print(exception)
+                        continue
+                if(len(buffered_tasks)>0):
+                    await asyncio.wait(buffered_tasks)
+                    buffered_tasks.clear()
 
 asyncio.run(main())
--- program.py
+++ program.py
@@ -11,15 +11,16 @@ influxdb_row_parser:IInfluxDBRowParser = BPlusInfluxDBRowParser()
 
 
 with ExecutionTimer() as execution_timer:
-   data = pandas.read_csv("data.csv",sep=",",header="infer")
-   for index,row in data.iterrows():
-      try:
-         point = influxdb_row_parser.Parse("TAVASCAN L02_09_03_2023_10h39m40ff_EXCEL-Daten",row,data.columns)
-         resp = writer.WritePoint(point)
-         if resp:
-            print(resp)
-      except Exception as exception:
-         print(exception)
-         continue
+   chunks = pandas.read_csv("data.csv",sep=",",header="infer",chunksize=10000)
+   for chunk in chunks:
+      for index,row in chunk.iterrows():
+         try:
+            point = influxdb_row_parser.Parse("TAVASCAN L02_09_03_2023_10h39m40ff_EXCEL-Daten",row,chunk.columns)
+            resp = writer.WritePoint(point)
+            if resp:
+               print(resp)
+         except Exception as exception:
+            print(exception)
+            continue
 
 
